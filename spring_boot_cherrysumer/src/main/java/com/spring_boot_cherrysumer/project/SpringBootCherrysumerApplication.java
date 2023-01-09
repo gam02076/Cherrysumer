cf58c967cbae02e641cfa488beca26390753db7a
gam02076 <jeongseobim521@gmail.com>
2023-01-09 13:53:59 +0900

    asdf

---
+++ spring_boot_cherrysumer/src/main/java/com/spring_boot_cherrysumer/project/SpringBootCherrysumerApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_cherrysumer.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCherrysumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCherrysumerApplication.class, args);
+	}
+
+}
