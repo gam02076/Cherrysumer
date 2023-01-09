694ff863f8049bba93558c33025edd3219ca86fe
PungTeaK <csb20048331@gmail.com>
2023-01-09 15:02:59 +0900

    이것저것 수정

--- spring_boot_cherrysumer/src/main/java/com/spring_boot_cherrysumer/project/SpringBootCherrysumerApplication.java
+++ spring_boot_cherrysumer/src/main/java/com/spring_boot_cherrysumer/project/SpringBootCherrysumerApplication.java
@@ -1,9 +1,13 @@
 package com.spring_boot_cherrysumer.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.spring_boot_cherrysumer.project;"})
+@MapperScan(basePackages= {"com.spring_boot_cherrysumer.project;"})
 public class SpringBootCherrysumerApplication {
 
 	public static void main(String[] args) {
---
+++ spring_boot_cherrysumer/src/main/java/com/spring_boot_cherrysumer/project/WebSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.spring_boot_cherrysumer.project;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.formLogin().disable()
+			.headers().frameOptions().disable();
+		
+		return http.build();
+	}
+}
