b10e86940865c3f253b0063b8a50218fe877eba6
parkeunvin <eunbin7372@naver.com>
2023-02-21 15:15:34 +0900

    글쓰기 수정, 딜페이지 수정중, 폰트 수정

--- spring_boot_cherrysumer/src/main/resources/static/js/customer.js
+++ spring_boot_cherrysumer/src/main/resources/static/js/customer.js
@@ -1,12 +1,21 @@
 /**
  * 
  */
- function customerCheck(memId) {
- var id=memId
+ 
+  $(document).ready(function(){
+  
+  $('#upBtn2').click(function(){
+   var id = document.getElementById('memIdd').value;
+  
+alert(id);
  if(id==null){
   alert("로그인을 해주세요.")
   location.href="/boardList";
   }else{
    location.href="/customerinsert";
   };
-};
+
+
+});// click
+});//완전 끝
+
